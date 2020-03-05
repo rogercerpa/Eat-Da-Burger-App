875e4dfef1c1bee28317e1ddca7737932c1f1995
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-03-04 20:07:55 -0500

    updated the connection js info, add info to the seeds sql file

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
 	port     : '3306',
 	user     : 'root',
 	password : 'root',
-	database : ''
+	database : 'eat_da_burger'
 });
 connection.connect((err) => {
 	if (err) {
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('veggie');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('mushroom');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('pineapple');
