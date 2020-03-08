f038b5389dbeec1f3be3ce491c2862ca668c4392
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-03-07 23:55:56 -0500

    updated the index js file to have functionality to all the buttons and form

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,30 @@
 $(() => {
-	$('.');
+	$('.change-devoured').on('click', function(event) {
+		const id = $(this).data('id');
+		const newDevoured = $(this).data('devoured');
+		const newDevouredState = {
+			devoured : newDevoured
+		};
+		$.ajax(`/api/burgers/${id}`, {
+			type : 'PUT',
+			data : newDevouredState
+		}).then(() => {
+			console.log('changed devoured to', newDevoured);
+			location.reload();
+		});
+	});
+
+	$('.create-form').on('submit', function(event) {
+		event.preventDefault();
+		const newBurger = {
+			burger_name : $('#input-field').val().trim()
+		};
+		$.ajax('/api/burgers', {
+			type : 'POST',
+			data : newBurger
+		}).then(() => {
+			console.log('added a new burger');
+			location.reload();
+		});
+	});
 });
